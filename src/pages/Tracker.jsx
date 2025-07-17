5d4d6b8e4a148b1d4d676032bf95073fd876198a
Ankush Bhanja <ankushbhanja8@gmail.com>
2025-07-17 09:25:45 +0530

    tracker bug fix

--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -58,13 +58,13 @@ function Tracker() {
         </div> :<div className='w-104 m-4 flex flex-col gap-2 justify-center items-center p-5 rounded-md bg-white text-black'>
             <div className='flex gap-2 items-center'><img src="/assets/finance_up.svg" alt="finance" className='size-10' /><h1 className='font-bold text-4xl'>Track offer process</h1></div>
             <div className='flex flex-col bg-gray-300 p-3 rounded-md font-bold'>
-                <div className='flex gap-2 justify-start items-center'><img src="/assets/reward.svg" alt="reward" className='size-4'/><p>Offer name: <span className='text-purple-700'>{steps?.[0].cName}</span></p></div>
-                <div className='flex gap-2 justify-start items-center'><img src="/assets/upi.svg" alt="reward" className='size-4'/><p>UPI ID: <span className='text-green-700'>{steps?.[0].upiId}</span></p></div>
+                <div className='flex gap-2 justify-start items-center'><img src="/assets/reward.svg" alt="reward" className='size-4'/><p>Offer name: <span className='text-purple-700'>{steps?.[0]?.cName}</span></p></div>
+                <div className='flex gap-2 justify-start items-center'><img src="/assets/upi.svg" alt="reward" className='size-4'/><p>UPI ID: <span className='text-green-700'>{steps?.[0]?.upiId}</span></p></div>
                 <div className='flex gap-2 justify-start items-center'><img src="/assets/time.svg" alt="reward" className='size-4'/><p>Last updated: <span className='text-red-600'>{date?.toLocaleString()}</span></p></div>
             </div>
             <div className='flex flex-col gap-2 justify-center items-center'>
                 <h2 className='font-semibold'>Reach to payout state to earn reward</h2>
-                {steps?.map((step)=>(<div className='flex gap-5 justify-between font-bold' key={step._id}><p className='bg-gray-400 flex items-center rounded-md px-2 py-1'>{step.goal}</p><div className='flex gap-1 items-center justify-center bg-[#0f5a2d62] px-2 py-1 rounded-md'><img src="/assets/check.svg" alt="" /><p>Tracked</p></div></div>))}
+                {steps && steps.length> 0?<>{steps?.map((step)=>(<div className='flex gap-5 justify-between font-bold' key={step._id}><p className='bg-gray-400 flex items-center rounded-md px-2 py-1'>{step.goal}</p><div className='flex gap-1 items-center justify-center bg-[#0f5a2d62] px-2 py-1 rounded-md'><img src="/assets/check.svg" alt="" /><p>Tracked</p></div></div>))}</>:(<p className='font-semibold text-red-600 text-xl px-5'>No tracking found please again later.</p>)}
             </div>
         </div>}
         
