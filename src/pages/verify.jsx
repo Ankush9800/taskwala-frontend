42db984f1bc2ea1b4d1f5657f871653a4c9ceda6
Ankush Bhanja <ankushbhanja8@gmail.com>
2025-07-29 18:00:59 +0530

    fix

--- src/pages/verify.jsx
+++ src/pages/verify.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import { Client, Account } from "appwrite";
 
 function verify() {
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('<PROJECT_ID>');
+
+const account = new Account(client);
+
+const urlParams = new URLSearchParams(window.location.search);
+const secret = urlParams.get('secret');
+const userId = urlParams.get('userId');
+
+const promise = account.updateVerification(userId, secret);
+
+promise.then(function (response) {
+    console.log(response); // Success
+}, function (error) {
+    console.log(error); // Failure
+});
+
   return (
     <div>verify</div>
   )
